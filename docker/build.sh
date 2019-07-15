0ea7d0152b04d9a01d914323617ebb8baef98e2a
Dinakar Guniguntala <dinogun@gmail.com>
2019-07-15 14:32:44 +0530

    Update gcc libs version for alpine.     Update all Dockerfiles to add maven cache layer.     Add build.sh logging and timestamping.

--- docker/build.sh
+++ docker/build.sh
@@ -15,25 +15,66 @@
 # limitations under the License.
 #
 
+ts=$(date "+%Y%m%d%H%M%S")
+BUILD_LOG="build-${ts}.log"
+
+function cleanup() {
+	docker container prune -f
+	docker image prune -f
+
+	docker rmi $(docker images | grep watsonex-springboot | awk '{ print $3 }')
+	echo
+}
+
+function timediff() {
+	ssec=`date --utc --date "$1" +%s`
+	esec=`date --utc --date "$2" +%s`
+
+	diffsec=$(($esec-$ssec))
+	echo $diffsec
+}
+
+function getdate() {
+	date "+%Y-%m-%d %H:%M:%S"
+}
+
+function log() {
+	echo $@ 2>&1 | tee -a ${BUILD_LOG}
+}
+
+function docker_build() {
+	image_name=$1
+	file_name=$2
+
+	sdate=$(getdate)
+	log -n "${image_name}: Building docker image..."
+	docker build --pull -t ${image_name} -f ${file_name} . 2>>${BUILD_LOG} >>${BUILD_LOG}
+	edate=$(getdate)
+	tdiff=$(timediff "$sdate" "$edate")
+	log "done"
+	log "${image_name}: Build took $((tdiff/60)) mins and $((tdiff%60)) secs"
+	log "============================================================================="
+}
+
+cleanup
+
 # Build all the variants
 
 # OpenJ9 Builds
-docker build --pull -t watsonex-springboot:openj9-v8.slim -f Dockerfile.openj9.v8.slim .
-docker build --pull -t watsonex-springboot:openj9-v8 -f Dockerfile.openj9.v8 .
-
-docker build --pull -t watsonex-springboot:openj9-v11.slim -f Dockerfile.openj9.v11.slim .
-docker build --pull -t watsonex-springboot:openj9-v11 -f Dockerfile.openj9.v11 .
-docker build --pull -t watsonex-springboot:openj9-v11.jlink -f Dockerfile.openj9.v11.jlink .
-docker build --pull -t watsonex-springboot:openj9-v11.jlink.scc -f Dockerfile.openj9.v11.jlink.scc .
+docker_build watsonex-springboot:openj9-v8.slim Dockerfile.openj9.v8.slim
+docker_build watsonex-springboot:openj9-v8 Dockerfile.openj9.v8
+docker_build watsonex-springboot:openj9-v11.slim Dockerfile.openj9.v11.slim
+docker_build watsonex-springboot:openj9-v11 Dockerfile.openj9.v11
+docker_build watsonex-springboot:openj9-v11.jlink Dockerfile.openj9.v11.jlink
+docker_build watsonex-springboot:openj9-v11.jlink.scc Dockerfile.openj9.v11.jlink.scc
 
 
 # Hotspot builds
-docker build --pull -t watsonex-springboot:hotspot-v8.slim -f Dockerfile.hotspot.v8.slim .
-docker build --pull -t watsonex-springboot:hotspot-v8 -f Dockerfile.hotspot.v8 .
+docker_build watsonex-springboot:hotspot-v8.slim Dockerfile.hotspot.v8.slim
+docker_build watsonex-springboot:hotspot-v8 Dockerfile.hotspot.v8
+docker_build watsonex-springboot:hotspot-v11.slim Dockerfile.hotspot.v11.slim
+docker_build watsonex-springboot:hotspot-v11 Dockerfile.hotspot.v11
 
-docker build --pull -t watsonex-springboot:hotspot-v11.slim -f Dockerfile.hotspot.v11.slim .
-docker build --pull -t watsonex-springboot:hotspot-v11 -f Dockerfile.hotspot.v11 .
-
-echo
-echo
-docker images | grep -e "TAG" -e "watsonex"
+log
+log
+docker images | grep -e "TAG" -e "watsonex" | sort --key=7 | tee -a ${BUILD_LOG}
